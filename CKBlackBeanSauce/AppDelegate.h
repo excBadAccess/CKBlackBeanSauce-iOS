4c7c7e430365ea07ac3ea626f74614e997bba9b3
Tony Borner <tonyunreal@hotmail.com>
2013-04-18 10:28:33 +0800

    Init Commit

---
+++ CKBlackBeanSauce/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  CKBlackBeanSauce
+//
+//  Created by Tony Borner on 4/18/13.
+//  Copyright (c) 2013 CkStudio. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ CKBlackBeanSauce/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  CKBlackBeanSauce
+//
+//  Created by Tony Borner on 4/18/13.
+//  Copyright (c) 2013 CkStudio. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
